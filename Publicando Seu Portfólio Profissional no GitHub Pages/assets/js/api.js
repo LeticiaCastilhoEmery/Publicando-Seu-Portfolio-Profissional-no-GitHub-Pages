d62362a676ce9d0f2551015bd0eb826d5300f3e8
Letícia Castilho do Emery <titacastilho@gmail.com>
2025-08-27 02:05:17 -0300

    Update api.js

--- Publicando Seu Portfólio Profissional no GitHub Pages/assets/js/api.js
+++ Publicando Seu Portfólio Profissional no GitHub Pages/assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/LeticiaCastilhoEmery/Publicando-Seu-Portfolio-Profissional-no-GitHub-Pages/refs/heads/main/Publicando%20Seu%20Portf%C3%B3lio%20Profissional%20no%20GitHub%20Pages/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/LeticiaCastilhoEmery/Publicando-Seu-Portfolio-Profissional-no-GitHub-Pages/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
